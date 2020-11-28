f1e89ec3ca38ded072eff000b28ec8a3a19d60b9
geeth22 <geethchandra22@gmail.com>
2020-11-29 00:52:42 +0530

    refactor:enter option to operate stack

--- stack.js
+++ stack.js
@@ -5,19 +5,28 @@ checkoption = function () {
     let isTerminated = true;
     while (isTerminated == true) {
         /** take choice input from the user */
-        let checkoption = readlineSync.question("\nEner your choice:-\n1.)push\n2.)pop\n3.)print\n4.)peek\n5.)exit\n");
+        let checkoption = readlineSync.question("\nEner your choice:-\n1.)push\n2.)pop\n3.)print\n4.)peek\n5.)length\n6.)reverse\n7.)exit\n");
         switch (checkoption) {
-            case '1': let element = readlineSync.question("Enter the element:");
+            case '1': 
+		let element = readlineSync.question("Enter the element:");
                 Utility.push(element);
                 break;
-            case '2': Utility.pop();
+            case '2': 
+		Utility.pop();
                 break;
-            case '3': Utility.print();
+            case '3': 
+		Utility.print();
                 break;
 	    case '4': 
 		let peek = Utility.peek();
 		console.log(peek);    
-            case '5': isTerminated = false;
+	    case '5': 
+		let lengthofstack = Utility.length();
+		console.log(lengthofstack);
+	    case '6':
+		Utility.reverse();
+		break;
+            case '7': isTerminated = false;
                 break;
             default: console.log("please enter correct option");
         }
