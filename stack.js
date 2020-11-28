212232fb260d569fc0ced5f2ce07da3658a63668
geeth22 <geethchandra22@gmail.com>
2020-11-29 00:35:58 +0530

    refactor:to choose the option

--- stack.js
+++ stack.js
@@ -1,5 +1,23 @@
 const Utility = require('./Utility');
 let readlineSync = require('readline-sync');
-let element = readlineSync.question("Enter Integer: ");
-Utility.push(element);
-Utility.pop();
+
+checkoption = function () {
+    let isTerminated = true;
+    while (isTerminated == true) {
+        /** take choice input from the user */
+        let checkoption = readlineSync.question("\nEner your choice:-\n1.)push\n2.)pop\n3.)print\n4.)exit\n");
+        switch (checkoption) {
+            case '1': let element = readlineSync.question("Enter the element:");
+                Utility.push(element);
+                break;
+            case '2': Utility.pop();
+                break;
+            case '3': Utility.print();
+                break;    
+            case '4': isTerminated = false;
+                break;
+            default: console.log("please enter correct option");
+        }
+    }
+}
+checkoption();
