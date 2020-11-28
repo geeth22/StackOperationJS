8cdacd58f7d5ab908bc4bca0d5a16765ac74b65e
geeth22 <geethchandra22@gmail.com>
2020-11-29 00:25:38 +0530

    added: function pop to remove top element from stack

--- utility.js
+++ utility.js
@@ -7,5 +7,14 @@ class Stack {
       this.data[this.top] = element;
       this.top = this.top + 1;
     }
+    isEmpty = () => {
+        return this.top === 0;
+    }
+    pop() {
+        if (this.isEmpty() === false) {
+            this.top = this.top - 1;
+            return this.data.pop(); // removes the last element
+        }
+    }
 }
 module.exports = new Stack();
